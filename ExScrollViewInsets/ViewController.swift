21d57f584c709b64241d036273f5ead58882880a
jake <palatable7@naver.com>
2023-01-31 22:53:29 +0900

    Init

---
+++ ExScrollViewInsets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExScrollViewInsets
+//
+//  Created by 김종권 on 2023/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
