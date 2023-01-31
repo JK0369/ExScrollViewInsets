5cad6b809e1c26a93542c1840b98a7f0dea45f33
jake <palatable7@naver.com>
2023-02-01 01:39:48 +0900

    Implement

--- ExScrollViewInsets/ViewController.swift
+++ ExScrollViewInsets/ViewController.swift
@@ -12,7 +12,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        scrollView.contentInset = .init(top: 120, left: 30, bottom: 0, right: 0)
         scrollView.contentInset = .init(top: 120, left: 30, bottom: 0, right: 0)
+//        scrollView.contentInsetAdjustmentBehavior = .automatic
+//        scrollView.contentInsetAdjustmentBehavior = .scrollableAxes
+//        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.contentInsetAdjustmentBehavior = .always
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print(scrollView.contentOffset) // (-30.0, -179.0)
+        print(scrollView.adjustedContentInset) // UIEdgeInsets(top: 179.0, left: 30.0, bottom: 34.0, right: 0.0)
     }
 }
-
